bb236336a4e1837d9c0c079d6b7bde7e90bf64aa
Jesus Rodriguez Barrera <jeroba@gmail.com>
2016-03-28 00:47:10 -0600

    commit

--- BuscadorLibrosTabla/MasterViewController.swift
+++ BuscadorLibrosTabla/MasterViewController.swift
@@ -9,131 +9,26 @@
 import UIKit
 import CoreData
 
-struct Seccion{
-    var nombreLibro: String
-    var isbn: String
-    var autores: String
-    var portada: UIImage
-    
-    init(nombreLibro: String, isbn: String, autores: String, portada: UIImage){
-        self.nombreLibro = nombreLibro
-        self.isbn = isbn
-        self.autores = autores
-        self.portada = portada
-    }
-}
 
-class MasterViewController: UITableViewController, NSFetchedResultsControllerDelegate {
+class MasterViewController: UITableViewController {
 
     var detailViewController: DetailViewController? = nil
     var managedObjectContext: NSManagedObjectContext? = nil
-    var isbn = String()
-    var nombreLibro = String()
-    var secciones = [Seccion]()
+    
+    var libros = [LiBroOpenLibrary]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        if let split = self.splitViewController {
+            let controllers = split.viewControllers
+            self.detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
+        }
     }
 
     override func viewWillAppear(animated: Bool) {
         self.clearsSelectionOnViewWillAppear = self.splitViewController!.collapsed
         super.viewWillAppear(animated)
-    }
-    
-    
-    @IBAction func buscar(sender: UITextField) {
-        sincrono(sender.text!)
-        insertNewObject()
-        sender.text = nil
-        sender.resignFirstResponder()
-    }
-    
-    func sincrono(isbn:String){
-        let urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:\(isbn)"
-        let url = NSURL(string: urls)
-        let datos:NSData? = NSData(contentsOfURL: url!)
-        if datos == nil{
-            let alert = UIAlertController(title: "Error", message: "La conexión a internet parece estar desactivada.", preferredStyle: .Alert)
-            let cancelAction = UIAlertAction(title: "Ok", style: .Cancel, handler: nil)
-            alert.addAction(cancelAction)
-            self.presentViewController(alert, animated: true, completion: nil)
-        }else{
-            let texto = NSString(data: datos!, encoding: NSUTF8StringEncoding)
-            if texto == "{}"{
-                let alert = UIAlertController(title: "Error", message: "Libro no encontrado.", preferredStyle: .Alert)
-                let cancelAction = UIAlertAction(title: "Ok", style: .Cancel, handler: nil)
-                alert.addAction(cancelAction)
-                self.presentViewController(alert, animated: true, completion: nil)
-            }else{
-                do{
-                    let json = try NSJSONSerialization.JSONObjectWithData(datos!, options: NSJSONReadingOptions.MutableLeaves)
-                    
-                    let isbnJson = json as! NSDictionary
-                    let isbnQuery = isbnJson["ISBN:\(isbn)"] as! NSDictionary
-                    let autores = isbnQuery["authors"] as! [NSDictionary]
-                    let portada = isbnQuery["cover"] as! NSDictionary?
-                    
-                    var portadaImagen = UIImage()
-                    
-                    if portada == nil{
-                        
-                    }else{
-                        let portadaMedium = portada!["medium"] as! NSString as String
-                        
-                        if let url  = NSURL(string: portadaMedium),
-                            data = NSData(contentsOfURL: url)
-                        {
-                            portadaImagen = UIImage(data: data)!
-                        }
-                    }
-                    
-                    
-                    let titulo = isbnQuery["title"] as! NSString as String
-                    var autoresString = ""
-                    for autor in autores{
-                        if autoresString == ""{
-                            autoresString += autor["name"] as! NSString as String
-                        }else{
-                            autoresString += ", "
-                            autoresString += autor["name"] as! NSString as String
-                        }
-                    }
-                    
-                    let seccion = Seccion(nombreLibro: titulo, isbn: isbn, autores: autoresString, portada: portadaImagen)
-                    secciones.append(seccion)
-                }catch _{
-                    
-                }
-            }
-        }
-    }
-
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    func insertNewObject() {
-        let context = self.fetchedResultsController.managedObjectContext
-        let entity = self.fetchedResultsController.fetchRequest.entity!
-        let newManagedObject = NSEntityDescription.insertNewObjectForEntityForName(entity.name!, inManagedObjectContext: context)
-             
-        // If appropriate, configure the new managed object.
-        // Normally you should use accessor methods, but using KVC here avoids the need to add a custom class to the template.
-        newManagedObject.setValue(NSDate(), forKey: "timeStamp")
-             
-        // Save the context.
-        do {
-            try context.save()
-        } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            //print("Unresolved error \(error), \(error.userInfo)")
-            abort()
-        }
     }
 
     // MARK: - Segues
@@ -141,9 +36,11 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "showDetail" {
             if let indexPath = self.tableView.indexPathForSelectedRow {
-            let object = self.fetchedResultsController.objectAtIndexPath(indexPath)
+                //let object = self.fetchedResultsController.objectAtIndexPath(indexPath)
                 let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
-                controller.detailItem = object
+                //Enviamos el objeto tipo LibroOpenlibrary seleccionado a la vista ViewDetail
+                controller.detailItem = self.libros[indexPath.row]
+                //...
                 controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
                 controller.navigationItem.leftItemsSupplementBackButton = true
             }
@@ -153,115 +50,49 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
     // MARK: - Table View
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return self.secciones.count
+        //return self.fetchedResultsController.sections?.count ?? 0
+        return 1
     }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.secciones.count
+        //let sectionInfo = self.fetchedResultsController.sections![section]
+        //return sectionInfo.numberOfObjects
+        return self.libros.count
     }
-
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
-        let object = self.fetchedResultsController.objectAtIndexPath(indexPath) as! NSManagedObject
-        self.configureCell(cell, withObject: object)
+        //let object = self.fetchedResultsController.objectAtIndexPath(indexPath) as! NSManagedObject
+        //self.configureCell(cell, withObject: object)
+        cell.textLabel?.text = self.libros[indexPath.row].titulo
         return cell
     }
-
+    
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
-        return true
+        return false
     }
-
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            let context = self.fetchedResultsController.managedObjectContext
-            context.deleteObject(self.fetchedResultsController.objectAtIndexPath(indexPath) as! NSManagedObject)
-                
-            do {
-                try context.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                //print("Unresolved error \(error), \(error.userInfo)")
-                abort()
+    
+    @IBAction func cancelToBooksViewController(segue:UIStoryboardSegue) {
+        print ("Se ha cancelado la búsqueda")
+    }
+    
+    @IBAction func saveBookDetail(segue:UIStoryboardSegue) {
+        if let busquedaISBNViewController = segue.sourceViewController as? BusquedaISBNViewController {
+            //añadir un nuevo libro al array
+            let libro = busquedaISBNViewController.libro
+            if libro.titulo != "" {
+                libros.append(libro)
+                //actualizar la vista de la tabla
+                let indexPath = NSIndexPath(forRow: libros.count-1, inSection: 0)
+                tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+            }else{
+                print ("No se ha añadido ningún libro a la lista")
             }
         }
+        
     }
 
-    func configureCell(cell: UITableViewCell, withObject object: NSManagedObject) {
-        cell.textLabel!.text = object.valueForKey("timeStamp")!.description
-    }
-
-    // MARK: - Fetched results controller
-
-    var fetchedResultsController: NSFetchedResultsController {
-        if _fetchedResultsController != nil {
-            return _fetchedResultsController!
-        }
-        
-        let fetchRequest = NSFetchRequest()
-        // Edit the entity name as appropriate.
-        let entity = NSEntityDescription.entityForName("Event", inManagedObjectContext: self.managedObjectContext!)
-        fetchRequest.entity = entity
-        
-        // Set the batch size to a suitable number.
-        fetchRequest.fetchBatchSize = 20
-        
-        // Edit the sort key as appropriate.
-        let sortDescriptor = NSSortDescriptor(key: "timeStamp", ascending: false)
-        
-        fetchRequest.sortDescriptors = [sortDescriptor]
-        
-        // Edit the section name key path and cache name if appropriate.
-        // nil for section name key path means "no sections".
-        let aFetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.managedObjectContext!, sectionNameKeyPath: nil, cacheName: "Master")
-        aFetchedResultsController.delegate = self
-        _fetchedResultsController = aFetchedResultsController
-        
-        do {
-            try _fetchedResultsController!.performFetch()
-        } catch {
-             // Replace this implementation with code to handle the error appropriately.
-             // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. 
-             //print("Unresolved error \(error), \(error.userInfo)")
-             abort()
-        }
-        
-        return _fetchedResultsController!
-    }    
-    var _fetchedResultsController: NSFetchedResultsController? = nil
-
-    func controllerWillChangeContent(controller: NSFetchedResultsController) {
-        self.tableView.beginUpdates()
-    }
-
-    func controller(controller: NSFetchedResultsController, didChangeSection sectionInfo: NSFetchedResultsSectionInfo, atIndex sectionIndex: Int, forChangeType type: NSFetchedResultsChangeType) {
-        switch type {
-            case .Insert:
-                self.tableView.insertSections(NSIndexSet(index: sectionIndex), withRowAnimation: .Fade)
-            case .Delete:
-                self.tableView.deleteSections(NSIndexSet(index: sectionIndex), withRowAnimation: .Fade)
-            default:
-                return
-        }
-    }
-
-    func controller(controller: NSFetchedResultsController, didChangeObject anObject: AnyObject, atIndexPath indexPath: NSIndexPath?, forChangeType type: NSFetchedResultsChangeType, newIndexPath: NSIndexPath?) {
-        switch type {
-            case .Insert:
-                tableView.insertRowsAtIndexPaths([newIndexPath!], withRowAnimation: .Fade)
-            case .Delete:
-                tableView.deleteRowsAtIndexPaths([indexPath!], withRowAnimation: .Fade)
-            case .Update:
-                self.configureCell(tableView.cellForRowAtIndexPath(indexPath!)!, withObject: anObject as! NSManagedObject)
-            case .Move:
-                tableView.moveRowAtIndexPath(indexPath!, toIndexPath: newIndexPath!)
-        }
-    }
-
-    func controllerDidChangeContent(controller: NSFetchedResultsController) {
-        self.tableView.endUpdates()
-    }
 
     /*
      // Implementing the above methods to update the table view in response to individual changes may have performance implications if a large number of changes are made simultaneously. If this proves to be an issue, you can instead just implement controllerDidChangeContent: which notifies the delegate that all section and object changes have been processed.
